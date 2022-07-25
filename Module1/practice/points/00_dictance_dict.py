9a4d1618855aa084b7fa1fe9ef10d8d3adedc88d
Anna12s <109950497+Anna12s@users.noreply.github.com>
2022-07-25 12:00:34 +0300

    Update 00_dictance_dict.py

--- Module1/practice/points/00_dictance_dict.py
+++ Module1/practice/points/00_dictance_dict.py
@@ -4,11 +4,12 @@ def distance(p1, p2):
     """
     # TODO: напишите тело функции
 
-
 # Даны две точки на координатной плоскости
 point1 = {"x": 2, "y": 5}
 point2 = {"x": -2, "y": 4}
-
+AC = point2[1]["x"] - point1[1]["x"]
+BC = point2[1]["y"] - point1[1]["y"]
+AB = (AC * AC + BC * BC) ** 0.5
 dist = distance(point1, point2)
 
 print("Расстояние между точками = ", dist)
